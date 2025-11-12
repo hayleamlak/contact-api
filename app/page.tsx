a39f7ced56a5fe35ad3f8679f1f3b513b5b2909d
Hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-11-12 08:37:49 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800">
-        Welcome to My Website
+        Welcome to My Website,my name is hayleamalk tesfaye 
+      <p>i am a computer science student</p>
       </h1>
     </div>
   );
